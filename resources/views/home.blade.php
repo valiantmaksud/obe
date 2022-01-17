82c8117316f6c4d21df800ee414768cd74bcf4a4
valiantboymaksud <valiantmaksud@gmail.com>
2022-01-17 22:37:55 +0600

    update role

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,7 +27,8 @@
                         <div class="row">
                             <div class="col-xs-12">
 
-                                <h2 class="text-center" style="margin-top:20vh">{{ auth()->user()->userrole }}</h2>
+                                <h2 class="text-center" style="margin-top:20vh">
+                                    {{ auth()->user()->username }} ({{ auth()->user()->userrole }})</h2>
                             </div>
                         </div>
                     </div>
